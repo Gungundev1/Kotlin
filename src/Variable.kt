679f5fb6689c4416682a935e2cc147a4a2fa44c2
Gungun agrawal <agrawalgungun859@gmail.com>
2024-07-08 19:38:52 +0530

    first commit

---
+++ src/Variable.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var  n =10// var type is mutable
+    println("value of n is: $n")
+    val userId= 1234 // val type is immutable
+    var userName="Gungun"// automatically string type
+    var num:Double=34.5// double type
+    println("value of n is: $n")
+    println("userId is: $userId")
+    println("userName is: $userName ")
+    println("value of n is: $num")
+    // sum of two numbers
+    println("sum of $n and $num is: ${n+num} ")
+
+
+}
