e7f56cf26c66b1e3dbba0bbbc3b91ccc0b9e3b52
Gungun agrawal <agrawalgungun859@gmail.com>
2024-07-10 17:41:24 +0530

    if-else

---
+++ src/ControlFlowStatements/If_else.kt
@@ -0,0 +1,9 @@
+package ControlFlowStatements
+fun main(){
+    println("Enter any number")
+    val num = readln().toInt()
+    if (num>0)
+        println("No. is positive")
+    else
+        println("NO. is negative")
+}
